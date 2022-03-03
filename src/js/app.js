f94963e017dd87d65c747555bac2c63a796d2386
engjell13 <engjellm2001@gmail.com>
2022-03-03 19:43:15 +0100

    fixed header, added scroll indicator, added loader

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,16 @@ require("../scss/style.scss");
 $(".scene").each((index, element) => {
   new Parallax(element);
 });
+
+$(window).on("scroll", () => {
+  const scroll = $(window).scrollTop();
+  if (scroll > 0) {
+    $("header").addClass("isScrolling");
+  } else {
+    $("header").removeClass("isScrolling");
+  }
+});
+
+$(window).on("load", () => {
+  $("#loader").slideUp();
+});
